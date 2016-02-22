b5718340dcf9030f5c36a0d8254925a6bdd74e92
JuggerNaut <ersah_ihlamur@hotmail.com>
2016-02-22 09:57:35 +0200

    Update parallax.js

--- src/parallax.js
+++ src/parallax.js
@@ -6,9 +6,31 @@
             speed     : 3,
             direction : 'down'
         }, options);
-		
+
+		var win = $(window),
+			scrollAmount,
+			top,
+			height,
+			scrollAmount,
+			parallaxAmount;
+
         return this.each( function() {
-			
+
+			//variables
+			 var item = $(this);
+
+			//on scroll event updating variables
+			function valueUpdater(){
+					scrollAmount = win.scrollTop()
+					top = item.parent().offset().top;
+					height = item.parent().outerHeight();
+					scrollAmount = win.scrollTop();
+					parallaxAmount = (((scrollAmount+window.outerHeight) - top)/settings.speed);
+					if (settings.direction == "up"){
+						parallaxAmount = parallaxAmount*(-1);
+					}
+				}
+
 			// requestAnimationFrame prefixer
 		var scroll = window.requestAnimationFrame ||
 			 window.webkitRequestAnimationFrame ||
@@ -16,46 +38,29 @@
 			 window.msRequestAnimationFrame ||
 			 window.oRequestAnimationFrame ||
 			 // IE Fallback, you can even fallback to onscroll
-			 function(callback){ window.setTimeout(callback, 1000/30) };
-			
-			//variables
-			 var item = $(this),
-				scrollAmount = $(window).scrollTop(),
-				top = item.offset().top,
-				height = item.outerHeight(),
-				parallaxAmount = (((scrollAmount+window.outerHeight) - top)/settings.speed);
+			 function(callback){ window.setTimeout(callback, 1000/60) };
 
-				//scroll event for updating variable values
-				$(window).on('scroll',function(){
-					top = item.parent().offset().top;
-					height = item.parent().outerHeight();
-					scrollAmount = $(window).scrollTop();
-					parallaxAmount = (((scrollAmount+window.outerHeight) - top)/settings.speed);
-					if (settings.direction == "up"){
-						parallaxAmount = parallaxAmount*(-1);
-					}
-				});
+			var prevScroll = win.scrollTop();
 
 			//loop
-			var prevScroll = $(window).scrollTop();
-			
 			var animator = function () {
+				valueUpdater();
 				if (prevScroll != scrollAmount){
 					item.each(function(){
 						if (scrollAmount+window.outerHeight > top && scrollAmount < top+height && window.outerWidth > 1200){
 						 this.style.transform = 'translate3d(0,'+parallaxAmount+'px,0)';
 						 this.style.OTransform = 'translate3d(0,'+parallaxAmount+'px,0)';
-						 this.style.msTransform = 'translate3d(0,'+parallaxAmount+'px,0)';   
+						 this.style.msTransform = 'translate3d(0,'+parallaxAmount+'px,0)';
 						 this.style.MozTransform = 'translate3d(0,'+parallaxAmount+'px,0)';
 						 this.style.WebkitTransform = 'translate3d(0,'+parallaxAmount+'px,0)';
 						}
 					});
 					prevScroll = $(window).scrollTop();
 				}
-				
+
 				scroll(animator);
 			};
-			
+
 			function backgroundAdjust(height){
 				if(window.outerWidth > 1200) {
 					item.height(height);
@@ -67,7 +72,7 @@
 					item.css('transform','none');
 				}
 			}
-			
+
 			var url = item.css('background-image').replace('url(', '').replace(')', '').replace("'", '').replace('"', '').replace('"', '');
 			var bgImg = $('<img />');
 			bgImg.hide();
@@ -77,19 +82,24 @@
 				backgroundAdjust(imgHeight);
 				bgImg.remove();
 			});
-			
+
 			$(window).on('resize', function(){
 				backgroundAdjust(imgHeight);
 			});
 
 			item.append(bgImg);
 			bgImg.attr('src', url);
-			
+
 			//parent item defaults
 			item.parent().css({
 				overflow:'hidden',
 				position: 'relative'
 			});
+
+			setTimeout(function(){
+				valueUpdater();
+				scroll(animator);
+			},200);
 
 			//parallax item css styles
 			if (settings.direction == 'down'){
@@ -103,7 +113,7 @@
 					bottom: 'auto'
 				});
 			}
-			
+
            scroll(animator);
         });
     }
